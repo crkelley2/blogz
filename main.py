7debd3f5bfac8b49101925a84658d01310d2e7f0
Carmen Kelley <crkelley2@comcast.net>
2019-04-27 13:12:01 -0500

    Work on /newpost dealing with input, validation, redirect

--- main.py
+++ main.py
@@ -14,10 +14,16 @@ class Blog(db.Model):
     body = db.Column(db.String(500))
     completed = db.Column(db.Boolean) 
 
-    def __init__(self, name):
+    def __init__(self, name, body):
         self.name = name
         self.body = body
         self.completed = False
+
+    def is_valid(self):
+        if self.name and self.body:
+            return True
+        else:
+            return False
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -29,8 +35,28 @@ def blog():
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def newpost():
+    new_post_name = ""
+    new_post_body = ""
+    new_url = ""
 
-    return render_template('newpost.html')
+    if request.method == 'POST':
+        new_post_name = request.form['name']
+        new_post_body = request.form['body']
+        new_post = Blog(new_post_name, new_post_body)
+
+        if new_post.is_valid():
+            new_url = "/blog?id=" + str(new_post.id)
+            return redirect(new_url)
+        #TODO Need to commit to database and work on: 
+        #validation, 
+        #which showed flash message error (need in html?)
+        #and need to set secret key.
+        else:
+            flash("You must enter both a Name and content for your new Blog post")
+
+
+    else:
+        return render_template('newpost.html', new_post_name=new_post_name, new_post_body=new_post_body)
 
 if __name__ == '__main__':
     app.run()
