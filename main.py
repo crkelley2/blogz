f4af77d17a8414392b834ba9db4e4a56095d3b70
Carmen Kelley <crkelley2@comcast.net>
2019-05-06 20:57:30 -0500

    Refactor and Add User Class, Handlers and html for Login, Signup - Login rendering but not yet functioning, Signup doing neither

--- main.py
+++ main.py
@@ -3,24 +3,112 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBuG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build-a-blog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogz@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'fakdakt0pmjda>adalkkjfla'
+
 
 class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
     body = db.Column(db.String(500))
-    completed = db.Column(db.Boolean) 
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, name, body):
+    def __init__(self, name, body, owner):
         self.name = name
         self.body = body
-        self.completed = False
+        self.owner = owner
 
-    
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='owner') 
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'signup']
+    if request.endpoint not in allowed_routes and 'email' not in session:
+        return redirect('/login')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form('username')
+        password = request.form('password')
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            session['username'] = username
+            flash("Logged in")
+            return redirect('/newpost')
+        elif user and user.password != password:
+            flash('The password you entered is incorrect', 'error')
+        elif not user:
+            flash('The username you entered does not exist', 'error')
+            
+    return render_template('login.html')
+#TODO Don't forget nav bar in login.html to "Create Account" directed to /signup page
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
+
+        usernameError = ""
+        passwordError = ""
+        verifyError = ""
+
+        if (not username) or (username.strip() == ""):
+            nameError = "You must enter a username"
+        elif (len(name) < 3) or (" " in name):
+            nameError = "Your name must contain 3 characters and no spaces"
+        
+        if (not password) or (password.strip() == ""):
+            passwordError = "You must enter a password"
+        elif (len(password) < 3) or (" " in password):
+            passwordError = "Your password must contain 3 characters and no spaces"
+        
+        if (not verify) or (verify.strip() == ""):
+            passwordError = "You must complete this field"
+        elif verify != password:
+            verifyError = "The passwords you entered do not match. Please try again"
+
+        if nameError or passwordError or verifyError:
+            return render_template("signup.html", title="SignUp", name=name, nameError=nameError, password="", passwordError=passwordError, verify="", verifyError=verifyError)
+
+        #TODO Add validation: DONE USING USER SIGNUP MAIN AND SIGNUP HTML...
+        #If pull code from user signup, do I change base html re flash messages???
+        #If no code from user signup, change signup html re flash messages
+
+        else:
+            existing_user = User.query.filter_by(username=username).first()
+
+            if not existing_user:
+                new_user = User(username, password)
+                db.session.add(new_user)
+                db.session.commit()
+                session['username'] = username
+                flash("Logged in")
+                return redirect('/newpost')
+            else:
+                flash('Username already exists')
+                return render_template('signup.html')
+
+    return render_template('signup.html')
+
+@app.route('/signup', methods=['GET'])
+def signup_page():
+    return render_template('signup.html')
+
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
     return render_template('blog.html')
@@ -36,6 +124,9 @@ def display_posts():
         compiled_posts = Blog.query.all()
         return render_template('blog.html', compiled_posts=compiled_posts)
 
+#TODO refactor newpost route handler function since
+#there is a new parameter (owner) to consider 
+#when creating a blog entry. Tried owner below in new_post =
 @app.route('/newpost', methods=['POST', 'GET'])
 def newpost():
     new_post_name = ""
@@ -45,7 +136,7 @@ def newpost():
     if request.method == 'POST':
         new_post_name = request.form['name']
         new_post_body = request.form['body']
-        new_post = Blog(new_post_name, new_post_body)
+        new_post = Blog(new_post_name, new_post_body, owner)
 
         if (not new_post_name) or (not new_post_body):
             flash("You must enter both a Name and content for your new Blog post")
@@ -57,8 +148,7 @@ def newpost():
 
             new_url = "/blog?id=" + str(new_post.id)
             return redirect(new_url)
-        #TODO Need to add to main blog page listing all blogs,
-        #including title as a link to the single blog page with get query for id
+        
     else:
         return render_template('newpost.html', new_post_name=new_post_name, new_post_body=new_post_body)
 
