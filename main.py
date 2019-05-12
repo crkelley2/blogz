e438dbac80bce4709c14ac1fa36a55b6ae310670
Carmen Kelley <crkelley2@comcast.net>
2019-05-11 21:21:30 -0500

    Link posts to current user and debug errors with login, signup, etc

--- main.py
+++ main.py
@@ -33,12 +33,12 @@ class User(db.Model):
 
 @app.before_request
 def require_login():
-    allowed_routes = ['login', 'signup']
+    allowed_routes = ['login', 'signup', 'blog', 'index']
     if request.endpoint not in allowed_routes and 'username' not in session:
         return redirect('/login')
 
 @app.route('/login', methods=['POST', 'GET']) #Messy but works (with old blogs page with all posts; still need to connect posts/owners, etc)
-def login(): #REFACTOR to deal with login errors separately with error messages indicated below!
+def login(): #REFACTORED to deal with login errors separately with error messages in instructions
     if request.method == 'GET':
         return render_template('login.html')
     elif request.method == 'POST':
@@ -48,26 +48,15 @@ def login(): #REFACTOR to deal with login errors separately with error messages 
         if users.count() == 1:
             user = users.first()
             if password == user.password:
-                session['user'] = user.username
+                session['username'] = username
                 flash('Welcome Back, '+user.username)
-                return redirect("/")
-        flash('bad username or password')
-        return redirect("/login")
-#TODO REMOVE THIS WHEN DONE! def login(): #POST /login not working yet; 500 error when "submit" with accurate info
-#    if request.method == 'POST':
-#        username = request.form('username')
-#       password = request.form('password')
-#       user = User.query.filter_by(username=username).first()
-#       if user and (User.password == password):
-#           session['username'] = username
-#           flash("Logged in")
-#           return render_template('newpost.html')#actually need to redirect but haven't succeeded with signup; getting error 500 here
-#       elif user and (User.password != password):
-#           flash('The password you entered is incorrect', 'error')
-#       elif not user:
-#           flash('The username you entered does not exist', 'error')
-            
-#   return render_template('login.html')
+                return redirect("/newpost")
+            else:
+                flash('The password you entered is not correct')
+                return redirect("/login")
+        if users.count() == 0:
+            flash('The username you entered does not exist')
+            return redirect("/login")
 
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
@@ -98,9 +87,8 @@ def signup():
         if usernameError or passwordError or verifyError:
             return render_template("signup.html", title="SignUp", username=username, usernameError=usernameError, password="", passwordError=passwordError, verify="", verifyError=verifyError)
 
-        #TODO Add validation: DONE USING USER SIGNUP MAIN AND SIGNUP HTML...
-        #If pull code from user signup, do I change base html re flash messages???
-        #If no code from user signup, change signup html re flash messages
+        #TODO Validation: DONE USING USER SIGNUP MAIN AND SIGNUP HTML...
+        #Do I need to change others re flash messages??? Can they co-exist?
 
         else:
             existing_user = User.query.filter_by(username=username).first()
@@ -111,17 +99,18 @@ def signup():
                 db.session.commit()
                 session['username'] = username
                 flash("Logged in")
-                return render_template('newpost.html')#TODO Fix this to render at /newpost rather than signup; could not get redirect to work!
+                return redirect("/newpost") # TODO Fix this to render at /newpost rather than signup; cannot get redirect to work; server errors!!
             else:
                 flash('Username already exists')
-                return render_template('signup.html')
+                return render_template('signup.html') #This WORKS but lost styles.css when it rendered??????
     else:
         return render_template('signup.html')
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=['POST', 'GET']) #TODO Refactor index per directions and logout to login, I think. Right now, it goes here w/o all posts.
 def index():
     return render_template('blog.html')
+
 
 @app.route('/blog', methods=['POST', 'GET'])
 def display_posts():
@@ -134,28 +123,24 @@ def display_posts():
         compiled_posts = Blog.query.all()
         return render_template('blog.html', compiled_posts=compiled_posts)
 
-#TODO refactor newpost route handler function since
+#DONE: refactor newpost route handler function since
 #there is a new parameter (owner) to consider 
-#when creating a blog entry. I put owner below in new_post argument but
 #need to define owner to be user in session and add owner argument to blog;
-#Also need to pass user logged in when gathering blog entries and single post for existing user...etc
+#TODO need to pass current user when gathering blog entries and single post for existing user...etc
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def newpost():
-    new_post_name = ""
-    new_post_body = ""
-    new_url = ""
-
+    
     if request.method == 'POST':
         new_post_name = request.form['name']
         new_post_body = request.form['body']
-        new_post = Blog(new_post_name, new_post_body, owner)
-
+        
         if (not new_post_name) or (not new_post_body):
             flash("You must enter both a Name and content for your new Blog post")
             return render_template('newpost.html', new_post_name=new_post_name, new_post_body=new_post_body)
     
         else:     
+            new_post = Blog(new_post_name, new_post_body, current_user())
             db.session.add(new_post)
             db.session.commit()
 
@@ -163,12 +148,19 @@ def newpost():
             return redirect(new_url)
         
     else:
-        return render_template('newpost.html', new_post_name=new_post_name, new_post_body=new_post_body)
+        return render_template('newpost.html')
+
+#This is working a/o 5/11 8:21 p.m., in the sense that post blog posts new posts
+#And they are added to the list of all blogs too.
+#I've not yet tried to list all posts by a single user separately though, so...
+def current_user(): 
+    owner = User.query.filter_by(username=session['username']).first()
+    return owner
 
 
 @app.route("/logout", methods=['POST'])
 def logout():
-    del session['user']
+    del session['username']
     return redirect("/")
 
 
